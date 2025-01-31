39ccb7ec2450d74ffd77eee48fa04079ba924798
Eason Wu <102417979+Simoniracle@users.noreply.github.com>
2025-01-31 17:56:13 +0800

    Update agent.py

--- agent.py
+++ agent.py
@@ -9,13 +9,10 @@ class Agent(object):
         self.role_profile = role_profile
         self.conv_history = []
         self.conv_history.append({"role":"system", "content":role_profile})
-        # self.conv_history.append({"role":"assistant", "content":role_profile})
-        # self.conv_history.append({"role":"user", "content":role_profile})
         self.memory = []
 
     def process_message(self, msg):
         self.conv_history.append({"role":"user", "content": msg})
-        # print("self.conv_history",self.conv_history)
         answer = self.llm.chat(self.conv_history)
         while(answer == '[error]'):
             answer = chat_with_history(self.conv_history)

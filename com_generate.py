6b59a2be7d5f84604079c9589ecdc1cf1bef99d2
Eason Wu <102417979+Simoniracle@users.noreply.github.com>
2025-01-31 16:46:38 +0800

    Update com_generate.py

--- com_generate.py
+++ com_generate.py
@@ -34,35 +34,6 @@ def _qwen15_32b_chat(messages):
     messages.append({'role':'assistant', 'content': out})
     return messages
 
-
-
-def _minimax(messages):
-    payload = json.dumps({
-        "model": "abab5.5-chat",
-        "messages": messages,
-        "stream": False,
-        "max_tokens": 6000,
-        "temperature": 0.7,
-        "top_p": 0.9
-    })
-
-    headers = {
-        'Authorization': f'Bearer {API_KEY}',
-        'Content-Type': 'application/json'
-    }
-    
-    try:
-        response = requests.request("POST", url, headers=headers, data=payload)
-        messages.append(json.loads(response.text)['choices'][0]['message'])
-        return messages
-    except:
-        print('Request id: %s, Status code: %s, error code: %s, error message: %s' % (
-            response.request_id, response.status_code,
-            response.code, response.message
-        ))
-        messages.append({'role':'assistant', 'content':'[error]'})
-        return messages
-
 def chat(prompt, sys_prompt=DEFAULT_PROMPT):
     messages = [
         {'role':'system', 'content':sys_prompt},
